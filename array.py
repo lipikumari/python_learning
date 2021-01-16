5a96b4279bd1076e5835d4f4b5e3e913eb083b6c
Lipi Kumari <47522378+lipikumari@users.noreply.github.com>
2021-01-16 22:46:20 +0530

    Add files via upload

---
+++ array.py
@@ -0,0 +1,19 @@
+#Python array
+#create an array
+data_type=["string","list","tuple","dict"]
+print(data_type)
+#Acess the element of an array
+x=data_type[1]
+print(x)
+#Length of an array
+x=len(data_type)
+print(x)
+#looping array elements
+for x in data_type:
+    print(x)
+#Adding array elements
+data_type.append("set")
+print(data_type)
+#Remove Array elements
+data_type.remove("set")
+print(data_type)
