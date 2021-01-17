8c6fb4573aaa8b1ff2bb63aebcb103d231f80618
Lipi Kumari <47522378+lipikumari@users.noreply.github.com>
2021-01-17 20:46:10 +0530

    Add files via upload

---
+++ math.py
@@ -0,0 +1,26 @@
+#Python math
+#Built-in math function
+a=min(5,7,9)
+b=max(5,7,9)
+print(a)
+print(b)
+#abs() function return absolute value
+a=abs(-49.66)
+print(a)
+#pow(a,b) function returns x to the power of y
+a=pow(7,8)
+print(a)
+#math.sqrt() method returns the square root of a number
+import math
+a=int(math.sqrt(225))
+print(a)
+#math.ceil() method rounds a number upwards to its nearest integer, and the math.floor() method rounds a number downwards to its nearest integer.
+import math
+a=math.ceil(7.5)
+b=math.floor(7.5)
+print(a)
+print(b)
+#math.pi return value of pi
+import math
+c=math.pi
+print(c)
